c9bc53abb6253000e08dbcf3c52a9a46ef71fac5
Tala-coder <muhammedtalic.it@gmail.com>
2022-06-08 06:26:47 +0200

    delete

--- backendold/putovanja/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from putovanja.models import Question
-
-# Register your models here
-admin.site.register(Question)
--- backendold/putovanja/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class PutovanjaConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'putovanja'
